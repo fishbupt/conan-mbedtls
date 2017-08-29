4b17a2e74555b054d089cac83d5d7295218e12d9
hongliax <hong-liang_xu@keysight.com>
2017-08-29 11:19:16 +0800

    Enable shared option for conan

--- conanfile.py
+++ conanfile.py
@@ -8,9 +8,8 @@ class MbedtlsConan(ConanFile):
     url = "<Package recipe repository url here, for issues about the package>"
     build_policy = "missing"
     settings = "os", "compiler", "build_type", "arch"
-    options = {"shared": [True, False],
-               "fPIC": [True, False]}
-    default_options = "shared=False", "fPIC=True"
+    options = {"shared": [True, False]}
+    default_options = "shared=False"
     generators = "cmake"
 
     def source(self):
@@ -24,13 +23,19 @@ conan_basic_setup()''')
 
     def build(self):
         cmake = CMake(self)
-        flags = "-DBUILD_SHARED_LIBS=ON" if self.options.shared else "-DBUILD_SHARED_LIBS=OFF"
-        flags += " -DENABLE_PROGRAMS=OFF -DENABLE_TESTING=OFF -DENABLE_ZLIB_SUPPORT=OFF"
-        flags += " -DCMAKE_INSTALL_PREFIX=%s" % self.package_folder
-        if self.settings.os != "Windows" and self.options.fPIC:
-            flags += " -DCMAKE_POSITION_INDEPENDENT_CODE=TURE"
-        self.run('cmake mbedtls %s %s' % (cmake.command_line, flags))
-        self.run("cmake --build . --target install %s" % cmake.build_config)
+        if self.options.shared:
+            cmake.definitions["USE_SHARED_MBEDTLS_LIBRARY"] = "ON"
+        else:
+            cmake.definitions["USE_STATIC_MBEDTLS_LIBRARY"] = "ON"
+
+        cmake.definitions["ENABLE_TESTING"] = "OFF"
+        cmake.definitions["ENABLE_PROGRAMS"] = "OFF"
+        cmake.definitions["ENABLE_ZLIB_SUPPORT"] = "OFF"
+        cmake.definitions["CMAKE_INSTALL_PREFIX"] = self.package_folder
+
+        cmake.configure(source_dir="mbedtls")
+        cmake.build()
+        cmake.install()
 
     def package_info(self):
         self.cpp_info.libs = ["mbedtls", "mbedx509", "mbedcrypto"]
